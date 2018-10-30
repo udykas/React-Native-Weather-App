eb1fccdd18cca4a0f61a06ab2d0e90ae7849a7e4
udykas <udykas@gmail.com>
2018-10-30 18:04:45 -0400

    Added and styled a text input component

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { 
   StyleSheet, 
   Text, 
-  View,
   KeyboardAvoidingView,
   Platform, 
   TextInput,
@@ -12,11 +11,19 @@ import {
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+      <KeyboardAvoidingView style={styles.container}>
         <Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>
         <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
         <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
-      </View>
+
+        <TextInput
+          autoCorrect={false}
+          placeholder="Search any city"
+          placeholderTextColor="white"
+          style={styles.textInput}
+          clearButtonMode="always"
+        />
+      </KeyboardAvoidingView>
     );
   }
 }
@@ -40,5 +47,15 @@ const styles = StyleSheet.create({
   },
   smallText: {
     fontSize: 18,
+  },
+  textInput: {
+    backgroundColor: '#666',
+    color: 'white',
+    height: 40,
+    width: 300,
+    marginTop: 20,
+    marginHorizontal: 20,
+    paddingHorizontal: 10,
+    alignSelf: 'center',
   },
 });
